abc7cf7948d68c304cb352d078ccdfdf68c2c3b7
Caroline Basta <caroline.basta1@ucalgary.ca>
2024-03-20 05:42:42 +0000

    Cleaning up tables

--- database-scripts/seed.sql
+++ database-scripts/seed.sql
@@ -11,10 +11,3 @@ VALUES
     ('Alex', 'alex@example.com', 'adminpassword', NOW()),
     ('Sara', 'sara@example.com', 'adminpassword2', NOW());
 
-INSERT INTO guest_activities (activity_type, post_id, created_at)
-VALUES 
-    ('view', 1, NOW()),
-    ('like', 2, NOW()),
-    ('comment', 3, NOW()),
-    ('view', 1, NOW()),
-    ('view', 2, NOW());
