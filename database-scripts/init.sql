1769ec05383dcb5af6c611dfb6fc0c89b8942dd4
Ryan Huynh <ryan.huynh@ucalgary.ca>
2024-03-21 05:06:50 +0000

    Revert "Merge branch 'feature-database_README' into 'main'"
    This reverts merge request !3

--- database-scripts/init.sql
+++ database-scripts/init.sql
@@ -1,4 +1,3 @@
--- Members are users with accounts, which give them more features that non-members would not have.
 CREATE TABLE members (
     member_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -6,8 +5,6 @@ CREATE TABLE members (
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Admins are special accounts that have special privileges for moderate the website.
 CREATE TABLE admins (
     admin_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -16,52 +13,44 @@ CREATE TABLE admins (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Posts refer to the basic content that members can upload for others to read.
+
 CREATE TABLE posts (
     post_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT, -- Refers to author
+    user_id INT,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
-
--- Likes are how members express their favour towards a post.
 CREATE TABLE likes (
     like_id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT, -- Post liked in question
-    user_id INT, -- The user who made the like
+    post_id INT,
+    user_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
-
--- Comments how members can express specific thoughts and opinions on posts.
 CREATE TABLE comments (
     comment_id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT, -- the post the comment was made on
-    user_id INT, -- the user who made the comment
+    post_id INT,
+    user_id INT,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
-
--- Reports are instances where a member brings attention to a post for some reason.
 CREATE TABLE reports (
     report_id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT, -- the post that was reported
-    reporter_id INT, -- the user that made the report
+    post_id INT,
+    reporter_id INT,
     reason TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
     FOREIGN KEY (reporter_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
-
--- Favourites are how users can keep track of specific posts so that they may revisit it.
 CREATE TABLE favorites (
     favorite_id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT, -- The favourited post
-    user_id INT, -- The user who favourited the post
+    post_id INT,
+    user_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
