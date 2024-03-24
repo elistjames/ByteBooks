11ee006cda42b4bbf137f7f1e755c9fcb0261bb0
ss1106 <sukriti.sharma@ucalgary.ca>
2024-03-24 17:32:42 -0600

    feat: add bottom bar for mobile

--- my-react-app/src/Components/HeaderBar.js
+++ my-react-app/src/Components/HeaderBar.js
@@ -6,42 +6,43 @@ import "./../App.css";
 import bytebooksLogo from "./../media/navbarLogo.png";
 
 const HeaderBar = () => {
+    const handleLogoClick = (e) => {
+        e.preventDefault();
+        window.location.href = '/';
+    };
 
     return (
-      <div className="top-nav">
-        <Navbar
-          fixed="top"
-          expand="lg"
-
-          variant="dark"
-          className="top-nav justify-content-between"
-        >
-            <div>#</div>
-          <div style={{
-              textAlign: "right"
-               }}>
-            <Navbar.Brand>
-              <Link to="/"><img 
-                  src={bytebooksLogo}
-                  alt="ByteBooks"
-                  className="logo-image"
-              />
-              </Link>
-            </Navbar.Brand>
-          </div>
-          <div style={{ marginRight: 22}}>
-            <Nav>
-              <Nav.Item className="sign-in-icon">
-                <Link to="/signin">
-                  <PiSignIn style={{ color: "white", transform: "scale(1.7)"}}/>
-                </Link>
-              </Nav.Item>
-            </Nav>
-          </div>
-        </Navbar>
-      </div>
+        <div className="top-nav">
+            <Navbar
+                fixed="top"
+                expand="lg"
+                variant="dark"
+                className="top-nav justify-content-between"
+            >
+                <div>#</div>
+                <div style={{ textAlign: "center", paddingLeft: "20px"}}>
+                    <Navbar.Brand>
+                        <a href="/" onClick={handleLogoClick}>
+                            <img 
+                                src={bytebooksLogo}
+                                alt="ByteBooks"
+                                className="logo-image"
+                            />
+                        </a>
+                    </Navbar.Brand>
+                </div>
+                <div>
+                    <Nav>
+                        <Nav.Item className="sign-in-icon">
+                            <Link to="/signIn">
+                                <PiSignIn style={{ color: "white", paddingRight: "10px", paddingTop: "2px", transform: "scale(1.8)" }} />
+                            </Link>
+                        </Nav.Item>
+                    </Nav>
+                </div>
+            </Navbar>
+        </div>
     );
-
 }
 
 export default HeaderBar;
