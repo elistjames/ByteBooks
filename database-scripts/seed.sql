aa6536a388e68cca50d9cd54b893b4531e2e3734
Eli-CUAVs <eli.st.james@canadianuavs.ca>
2024-03-25 19:28:29 -0600

    fix: init.sql removed email attribute

--- database-scripts/seed.sql
+++ database-scripts/seed.sql
@@ -1,11 +1,11 @@
 -- Seed
-INSERT INTO users (username, email, password, created_at)
+INSERT INTO users (username password, created_at)
 VALUES
-    ('emily_writer', 'emily@example.com', 'password1', NOW()),
-    ('jon_jones', 'john@example.com', 'password2', NOW()),
-    ('adam.help', 'adam@example.com', 'password3', NOW());
+    ('emily_writer', 'password1', NOW()),
+    ('jon_jones', 'password2', NOW()),
+    ('adam.help', 'password3', NOW());
 
-INSERT INTO admins (username, email, password, created_at)
+INSERT INTO admins (username, password, created_at)
 VALUES
-    ('alex_admin', 'alex@example.com', 'adminpassword1', NOW()),
-    ('sara_admin', 'sara@example.com', 'adminpassword2', NOW());
+    ('alex_admin', 'adminpassword1', NOW()),
+    ('sara_admin', 'adminpassword2', NOW());
