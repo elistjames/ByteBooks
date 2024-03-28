cc5fa809bcf732a4f183fc7df272f42aa67d0dff
CarolineBasta <caroline.basta1@ucalgary.ca>
2024-03-27 23:13:05 -0600

    fix: change toast message position

--- my-react-app/src/Components/ConfirmationCard/ConfirmationToast.js
+++ my-react-app/src/Components/ConfirmationCard/ConfirmationToast.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { Toast } from 'react-bootstrap';
+import './ConfirmationToast.css'; 
 
 const ConfirmationToast = ({ show, message, onClose }) => {
     return (
-        <Toast onClose={onClose} show={show} delay={3000} autohide
-            style={{
-                position: 'fixed',
-                top: 50, 
-                right: 15,
-                zIndex: 100,
-                backgroundColor: 'rgba(255, 255, 255, 1)',
-                borderRadius: '16px'
-            }}>
-            <Toast.Header closeButton={true} style={{ borderRadius: '16px 16px 0 0', backgroundColor: 'transparent' }}>
-                <div style={{ display: 'inline-flex', width: '100%', justifyContent: 'space-between' }}>
+        <Toast onClose={onClose} show={show} delay={3000} autohide className="confirmation-toast">
+            <Toast.Header closeButton={true} className="toast-header">
+                <div className="header-content">
                     <strong>Confirmation</strong>
                 </div>
             </Toast.Header>
-            <Toast.Body style={{ borderRadius: '0 0 16px 16px' }}>{message}</Toast.Body>
+            <Toast.Body className="toast-body">{message}</Toast.Body>
         </Toast>
     );
 };
