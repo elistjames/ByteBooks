d215eef4948dfa559cb052f1a8adcde3e870f240
Caroline Basta <caroline.basta1@ucalgary.ca>
2024-03-28 17:24:43 +0000

    feat: add delete account confirmation modal

--- my-react-app/src/Components/Profile.js
+++ my-react-app/src/Components/Profile.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import "./../index.css";
-import { Button } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import ContentCard from "./ContentCard/ContentCard";
+import './../index.css';
 import './MainPage/MainPage.css';
+import ConfirmationModal from './ConfirmationCard/ConfirmationModal'; 
 
 const Profile = ({ posts = [] }) => {
   const usersPosts = posts.filter(post => post.user_id === '@elistjames');
 
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+  const handleConfirmDelete = () => {handleCloseModal();
+  };
+
   return (
     <>
-
       <Card className="form-card rounded-custom">
         <Card.Body>
           <div className="mb-3">
             <div className="static-field mb-3">
               <Card.Title className="text-muted">Username</Card.Title>
-
               <Card.Text className="static-value">@elistjames</Card.Text>
             </div>
-            <div className="static-field mb-3">
-              <Card.Title className="text-muted">Email</Card.Title>
-
-              <Card.Text className="static-value">elistjames@gmail.com</Card.Text>
-            </div>
+              <Button variant="danger" className="mx-auto d-flex justify-content-around mb-3" onClick={handleShowModal}>
+                Delete Account
+              </Button>
           </div>
-          <Button variant="danger" className="mx-auto d-block">
-            Delete Account
-          </Button>
         </Card.Body>
       </Card>
-      <h2 className="posts-heading text-center ">Your Posted Content</h2>
+      <h2 className="posts-heading text-center">Your Posted Content</h2>
       <div className="posts">
         {usersPosts.map(post => (
           <ContentCard key={post.post_id} post={post} username="@elistjames" />
         ))}
       </div>
+      <ConfirmationModal
+        show={showModal}
+        onHide={handleCloseModal}
+        onConfirm={handleConfirmDelete}
+        message="Are you sure you want to delete your account?"
+      />  
     </>
   );
 };
