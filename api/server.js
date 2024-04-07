0dba026d288d3e68f0d02a42c76ef5a06b772b16
Caroline Basta <caroline.basta1@ucalgary.ca>
2024-04-07 08:59:34 +0000

    fix: fix a potential merge request

--- api/server.js
+++ api/server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/post');
-// const commentRoutes = require('./routes/viewPost');
+const commentRoutes = require('./routes/comment');
 const reportRoutes = require('./routes/report');
 const likeRoutes = require('./routes/like');
 const dislikeRoutes = require('./routes/dislike');
-const adminRoutes = require('./routes/adminroute'); 
-
+const userRoutes = require('./routes/user');
 const app = express();
 
 app.use(express.json());
+
 
 const corsOptions = {
     origin: 'http://localhost:3000'  // when running on docker, set to 8888, when testing locally, change port to 3000
 };
 app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT || 8080;
 
@@ -25,7 +26,7 @@ app.use('/auth', authRoutes);
 // route for content main page requests
 app.use('/post', postRoutes);
 // route for comment requests
-
+app.use('/comment', commentRoutes);
 // route for report requests
 app.use('/report', reportRoutes);
 // route for like requests
@@ -33,9 +34,9 @@ app.use('/like', likeRoutes);
 
 // route for dislike requests
 app.use('/dislike', dislikeRoutes);
+// route for user requests
+app.use('/user', userRoutes);
 
-// route for admin requests
-app.use('/admin', adminRoutes);
 
 app.get("/status", (request, response) => {
     const status = {
@@ -45,7 +46,9 @@ app.get("/status", (request, response) => {
     response.send(status);
 });
 
+
 // API listening on port 3306
 app.listen(PORT, () => {
     console.log("Server listening on PORT:", PORT);
-});
+})
+
