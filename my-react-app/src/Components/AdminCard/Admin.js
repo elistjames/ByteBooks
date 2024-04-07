b76db7618727055784c2d28f1a3e96812544f33f
Eli-CUAVs <eli.st.james@canadianuavs.ca>
2024-04-07 16:10:58 -0600

    fix: delete post and user from admin

--- my-react-app/src/Components/AdminCard/Admin.js
+++ my-react-app/src/Components/AdminCard/Admin.js
@@ -6,6 +6,8 @@ import ConfirmationModal from '../ConfirmationCard/ConfirmationModal';
 import ConfirmationToast from '../ConfirmationCard/ConfirmationToast';
 import ReportController from "../../Controllers/ReportController";
 import { v4 as uuidv4 } from 'uuid';
+import MainPageController from "../../Controllers/MainPageController";
+import UserController from "../../Controllers/UserController";
 
 const Admin = () => {
     const [reportedUsers, setReportedUsers] = useState([]);
@@ -14,7 +16,8 @@ const Admin = () => {
 
     const [activeTab, setActiveTab] = useState('users');
     const [modalDialog, setModalDialog] = useState(false);
-    const [currentItemId, setCurrentItemId] = useState(null);
+    const [currentUserId, setCurrentUserId] = useState(null);
+    const [currentPostId, setCurrentPostId] = useState(null);
     const [dialogMessage, setDialogMessage] = useState('');
     const [showToast, setShowToast] = useState(false);
     const [toastMessage, setToastMessage] = useState('');
@@ -26,51 +29,67 @@ const Admin = () => {
         })
     }, []);
 
-    const deleteItem = (itemId) => {
-        setCurrentItemId(itemId);
-        const detail = activeTab === 'users'
-            ? reportedUsers.find(user => user.user_id === itemId)?.userId
-            : reportedPosts.find(post => post.post_id === itemId)?.title;
-        const message = activeTab === 'users'
-            ? `Are you sure you want to ban the user "${detail}"?`
-            : `Are you sure you want to delete the post "${detail}"?`;
-        setDialogMessage(message);
+    const deletePost = (post) => {
+        setCurrentPostId(post.post_id);
+        setDialogMessage(`Are you sure you want to delete the post "${post.title}"?`);
+        setModalDialog(true);
+    };
+
+
+    const deleteUser = (user) => {
+        setCurrentUserId(user.reported_user_id);
+        setDialogMessage(`Are you sure you want to ban the user "${user.username}"?`);
         setModalDialog(true);
     };
 
     const handleDelete = () => {
         let itemName = '';
-        if (activeTab === 'users') {
-            const user = reportedUsers.find(user => user.user_id === currentItemId);
-            if(user) {
-                itemName = user.userId; 
-            }
-            setReportedUsers(prevUsers => prevUsers.filter(user => user.user_id !== currentItemId));
-            setToastMessage(`User "${itemName}" has been banned.`); 
-        } else {
-            const post = reportedPosts.find(post => post.post_id === currentItemId);
-            if(post) {
-                itemName = post.title; 
-            }
-            setReportedPosts(prevPosts => prevPosts.filter(post => post.post_id !== currentItemId));
-            setToastMessage(`Post "${itemName}" has been deleted.`); 
-        }
-        setShowToast(true);
         setModalDialog(false);
+        if (activeTab === 'users') {
+            const user = reportedUsers.find(user => user.reported_user_id === currentUserId);
+            if(user) {
+                UserController.deleteAccount(user.reported_user_id).then((response) => {
+                    const index = reportedUsers.indexOf(user);
+                    if(index < 0) {
+                        return;
+                    }
+                    setReportedUsers(reportedUsers.splice(index, 1));
+                    setToastMessage(`User has been banned.`);
+                    setShowToast(true);
+                }).catch((error) => {
+                    console.log(error);
+                });
+            }
+        } else {
+            const post = reportedPosts.find(post => post.post_id === currentPostId);
+            if(post) {
+                MainPageController.deletePost(post.post_id).then((response) => {
+                    const index = reportedPosts.indexOf(post);
+                    if(index < 0) {
+                        return;
+                    }
+                    setReportedPosts(reportedPosts.splice(index, 1));
+                    setToastMessage(`Post "${post.title}" has been deleted.`);
+                    setShowToast(true);
+                }).catch((error) =>{
+                    console.log(error);
+                });
+            }
+        }
     };
-    
+
     return (
         <Container className="mid">
             <Row className="justify-content-md-center">
                 <Col xs={12} sm={12} md={10} lg={8}>
                     <div className="tabs">
-                        <span 
-                            className={`tab ${activeTab === 'users' ? 'active' : ''}`} 
+                        <span
+                            className={`tab ${activeTab === 'users' ? 'active' : ''}`}
                             onClick={() => setActiveTab('users')}
                         >
                             Reported Users
                         </span>
-                        <span 
+                        <span
                             className={`tab ${activeTab === 'posts' ? 'active' : ''}`}
                             onClick={() => setActiveTab('posts')}
                         >
@@ -85,8 +104,8 @@ const Admin = () => {
                                         <ListGroup.Item className="d-flex justify-content-between my-2 rounded-pill">
                                             <span className="reports">{user.report_count}</span>
                                             <span className="userId">@{user.username}</span>
-                                            <Button variant="link" className="remove-user-btn" onClick={() => deleteItem(user.user_id)} title="Ban user">
-                                                <IoPersonRemove size="1.5em" className="remove-user-icon"/> 
+                                            <Button variant="link" className="remove-user-btn" onClick={() => deleteUser(user)} title="Ban user">
+                                                <IoPersonRemove size="1.5em" className="remove-user-icon"/>
                                             </Button>
                                         </ListGroup.Item>
                                     </div>
@@ -104,7 +123,7 @@ const Admin = () => {
                                         </div>
                                         <div className="post-actions">
                                             <Button variant="primary" size="sm" className="view-btn" title="View post">View</Button>
-                                            <IoTrashOutline className="remove-post-icon" onClick={() => deleteItem(post.post_id)} title="Delete post"/>
+                                            <IoTrashOutline className="remove-post-icon" onClick={() => deletePost(post)} title="Delete post"/>
                                         </div>
                                     </ListGroup.Item>
                                 ))}
